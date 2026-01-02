beca2f1bb7f57feceab538697723fed9d3ed196d
adarshkumar61 <adarshb2k61@gmail.com>
2026-01-02 20:24:05 +0530

    updated

--- basis_robot/basis_robot/parameter.py
+++ basis_robot/basis_robot/parameter.py
@@ -10,11 +10,14 @@ class ParamNode(Node):
 
        #creating param:
        self.time = self.create_timer(1.0, self.callback)
+      #This tells ROS 2: Every 1 second → call self.callback()
+
 
     def callback(self):
+        #Every 1 second ROS 2 executes:
         speed =  self.get_parameter('robot_speed').value
         self.get_logger().info(f'Robot speed is: {speed}')
-
+        
 def main(args = None):
     rclpy.init(args = args)
     node = ParamNode()
@@ -24,3 +27,7 @@ def main(args = None):
 
 if __name__ == '__main__':
     main()
+
+
+# to change parameter live :
+# ros2 param set /param_example robot_speed 2.5

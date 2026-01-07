64882b507c940c5fc3b0ad1ea6f10c1f9785b5b2
adarshkumar61 <adarshb2k61@gmail.com>
2026-01-07 20:39:01 +0530

    updated

--- basis_robot/basis_robot/action_server.py
+++ basis_robot/basis_robot/action_server.py
@@ -24,3 +24,32 @@ class actionserver(Node):
 # Feedback publisher
         self.get_logger().info('goal started..')
 
+        feedback = Fibonacci.Feedback()
+        result = Fibonacci.Result()
+        #Feedback & Result must be created manually
+        count = 0
+        target = goal_handle.request.order
+
+        while count < target:
+            count+= 1
+            feedback.sequence = [count]
+            goal_handle.publish_feedback(feedback)
+
+            self.get_logger().info(f'Counting: {count}')
+            rclpy.sleep(1)
+            if goal_handle.is_cancel_requested:
+                goal_handle.canceled()
+                self.get_logger().info('goal canceled')
+                return Fibonacci.Result()
+        goal_handle.succeed()
+        result.sequence = [count]
+        return result
+    
+def main(args = None):
+    rclpy.init(args=args)
+    node = ActionServer()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()

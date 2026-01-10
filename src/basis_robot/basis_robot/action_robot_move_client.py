5404b9403d777782d4978263313131c9a349843b
adarshkumar61 <adarshb2k61@gmail.com>
2026-01-10 20:00:58 +0530

    updated

--- src/basis_robot/basis_robot/action_robot_move_client.py
+++ src/basis_robot/basis_robot/action_robot_move_client.py
@@ -2,3 +2,25 @@ import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
 from example_interfaces.action import Fibonacci
+
+class RobotMoveAC(Node):
+    def __init__(self):
+        super().__init__('robot_move_client')
+        
+        self.action_client = ActionClient(
+            self,
+            Fibonacci,
+            'robot_move'
+        )
+
+        self.get_logger().info('Action Client Node has been started')
+        self.action_client.wait_for_server()
+        
+        goal = Fibonacci.Goal()
+        goal.order = 3
+
+        self.action_client.send_goal_async(
+            goal,
+            feedback_callback=self.feedback_callback
+        ).add_done_callback(self.goal_response_callback)
+

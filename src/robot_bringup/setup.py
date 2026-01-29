66845f932f7ced013c35a24b4ed1d5a83a7521de
adarshkumar61 <adarshb2k61@gmail.com>
2026-01-29 18:41:14 +0530

    updated setup file

--- src/robot_bringup/setup.py
+++ src/robot_bringup/setup.py
@@ -1,29 +1,25 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'robot_bringup'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
+
         ('share/' + package_name, ['package.xml']),
+
+        # Install launch files
+        ('share/' + package_name + '/launch',
+         ['launch/bringup.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='adarsh61',
     maintainer_email='adarshb2k61@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
-    entry_points={
-        'console_scripts': [
-        ],
-    },
+    description='Robot bringup package',
+    license='Apache-2.0',
 )

1117da5252cdc3c75862fdc37dc46004831ffffc
adarshkumar61 <adarshb2k61@gmail.com>
2026-01-30 08:59:47 +0530

    updated and tested successfully launch file running

--- src/robot_bringup/setup.py
+++ src/robot_bringup/setup.py
@@ -8,13 +8,10 @@ setup(
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
-         ['resource/' + package_name]),
-
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-
-        # Install launch files
         ('share/' + package_name + '/launch',
-         ['launch/bringup.launch.py']),
+            ['launch/bringup.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,

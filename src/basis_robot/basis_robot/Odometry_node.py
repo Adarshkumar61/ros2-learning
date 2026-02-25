82c4a8c237db14ce716a4a35647f258cd1b1e6a2
adarshkumar61 <adarshb2k61@gmail.com>
2026-02-25 09:21:46 +0530

    updated

--- src/basis_robot/basis_robot/Odometry_node.py
+++ src/basis_robot/basis_robot/Odometry_node.py
@@ -1,106 +1,124 @@
-# import rclpy
-# from rclpy.node import Node
-# from geometry_msgs.msg import Twist
-# from nav_msgs.msg import Odometry
-# import math
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+import math
 
 
-# class OdometryNode(Node):
+class OdometryNode(Node):
 
-#     def __init__(self):
-#         super().__init__('odometry_node')
+    def __init__(self):
+        super().__init__('odometry_node')
 
-#         # Subscriber to velocity commands
-#         self.subscription = self.create_subscription(
-#             Twist,
-#             '/cmd_vel',
-#             self.cmd_callback,
-#             10
-#         )
+        # Subscriber to velocity commands
+        self.subscription = self.create_subscription(
+            Twist,
+            '/cmd_vel',
+            self.cmd_callback,
+            10
+        )
 
-#         # Publisher for odometry
-#         self.odom_publisher = self.create_publisher(
-#             Odometry,
-#             '/odom',
-#             10
-#         )
+        # Publisher for odometry
+        self.odom_publisher = self.create_publisher(
+            Odometry,
+            '/odom',
+            10
+        )
 
-#         # Robot pose (initial position)
-#         self.x = 0.0
-#         self.y = 0.0
-#         self.theta = 0.0
+        # Robot pose (initial position)
+        self.x = 0.0
+        self.y = 0.0
+        self.theta = 0.0
 
-#         # Velocity storage
-#         self.v = 0.0
-#         self.omega = 0.0
+        # Velocity storage
+        self.v = 0.0
+        self.omega = 0.0
 
-#         # Time tracking
-#         self.last_time = self.get_clock().now()
+        # Time tracking
+        self.last_time = self.get_clock().now()
 
-#         # Timer for 5 Hz update (0.2 sec)
-#         self.timer = self.create_timer(0.2, self.update_odometry)
+        # Timer for 5 Hz update (0.2 sec)
+        self.timer = self.create_timer(0.2, self.update_odometry)
 
-#         self.get_logger().info("Odometry node started...")
-
-
-#     # Velocity callback
-#     def cmd_callback(self, msg):
-#         self.v = msg.linear.x
-#         self.omega = msg.angular.z
+        self.get_logger().info("Odometry node started...")
 
 
-#     # Timer function (runs at 50Hz)
-#     def update_odometry(self):
-
-#         current_time = self.get_clock().now()
-#         dt = (current_time - self.last_time).nanoseconds / 1e9
-#         self.last_time = current_time
-
-#         # Kinematic equations
-#         self.x += self.v * math.cos(self.theta) * dt
-#         self.y += self.v * math.sin(self.theta) * dt
-#         self.theta += self.omega * dt
-
-#         # Create Odometry message
-#         odom_msg = Odometry()
-
-#         odom_msg.header.stamp = current_time.to_msg()
-#         odom_msg.header.frame_id = "odom"
-
-#         # Position
-#         odom_msg.pose.pose.position.x = self.x
-#         odom_msg.pose.pose.position.y = self.y
-#         odom_msg.pose.pose.position.z = 0.0
-
-#         # Orientation (simplified: yaw only)
-#         # NOTE: Proper version uses quaternion conversion,
-#         #  but for simplicity we use a 2D approximation here    
-#         odom_msg.pose.pose.orientation.z = math.sin(self.theta / 2.0)
-#         odom_msg.pose.pose.orientation.w = math.cos(self.theta / 2.0)
-
-#         # Velocity
-#         odom_msg.twist.twist.linear.x = self.v
-#         odom_msg.twist.twist.angular.z = self.omega
-
-#         # Publish
-#         self.odom_publisher.publish(odom_msg)
-
-#         # Print current pose
-#         self.get_logger().info(
-#             f"x: {self.x:.3f}, y: {self.y:.3f}, theta: {self.theta:.3f}"
-#         )
+    # Velocity callback
+    def cmd_callback(self, msg):
+        self.v = msg.linear.x
+        self.omega = msg.angular.z
 
 
-# def main(args=None):
-#     rclpy.init(args=args)
-#     node = OdometryNode()
-#     rclpy.spin(node)
-#     node.destroy_node()
-#     rclpy.shutdown()
+    # Timer function (runs at 50Hz)
+    def update_odometry(self):
+
+        current_time = self.get_clock().now()
+        dt = (current_time - self.last_time).nanoseconds / 1e9
+        self.last_time = current_time
+
+        # Kinematic equations
+        self.x += self.v * math.cos(self.theta) * dt
+        self.y += self.v * math.sin(self.theta) * dt
+        self.theta += self.omega * dt
+
+        # Create Odometry message
+        odom_msg = Odometry()
+
+        odom_msg.header.stamp = current_time.to_msg()
+        odom_msg.header.frame_id = "odom"
+
+        # Position
+        odom_msg.pose.pose.position.x = self.x
+        odom_msg.pose.pose.position.y = self.y
+        odom_msg.pose.pose.position.z = 0.0
+
+        # Orientation (simplified: yaw only)
+        # NOTE: Proper version uses quaternion conversion,
+        #  but for simplicity we use a 2D approximation here 
+
+        # Think of quaternion like this:
+           # It’s just a mathematical container that stores:
+                # “Robot is rotated by θ radians around Z-axis.”   
+        odom_msg.pose.pose.orientation.z = math.sin(self.theta / 2.0)
+        odom_msg.pose.pose.orientation.w = math.cos(self.theta / 2.0)
+
+        # Velocity
+        #Speed comes from this:
+
+        odom_msg.twist.twist.linear.x = self.v
+        odom_msg.twist.twist.angular.z = self.omega
+
+        # Publish
+        self.odom_publisher.publish(odom_msg)
+
+        # Print current pose
+        self.get_logger().info(
+            f"x: {self.x:.3f}, y: {self.y:.3f}, theta: {self.theta:.3f}"
+        )
+        """Position → Where am I
+Orientation (theta) → Which way am I facing
+Velocity (v, omega) → How am I moving
+
+Movement depends on:  v and omega
+
+Orientation depends on:  theta
+
+omega → changes theta
+theta → determines quaternion
 
 
-# if __name__ == '__main__':
-#     main()
+"""
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = OdometryNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
 
 
 # """
@@ -119,109 +137,3 @@
 
 
 
-import rclpy
-import math
-from rclpy.node import Node
-from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist
-
-
-class odometrynode(Node):
-    def __init__(self):
-        super().__init__('odometry_node')
-
-        self.create_subscription(
-            Twist,
-            "/cmd_vel", 
-            self.cmd_callback,
-            10
-        )
-
-        self.create_publisher(
-            Odometry,
-            '/odom',
-            10
-        )
-
-        #initilizing robot state:
-        self.x = 0.0
-        self.y = 0.0
-        self.t = 0.0
-
-        self.v = 0.0
-        self.w = 0.0
-
-        self.back_timer = self.get_clock().now()
-
-        self.timer = self.create_timer(
-            0.2,
-            self.update_odom
-        )
-
-    def cmd_callback(self, msg):
-        self.v = msg.linear.x
-        self.w = msg.angular.z
-
-    def update_odom(self):
-
-        current_timer = self.get_clock().now()
-        dt = (self.back_timer - current_timer).nanoseconds / 1e9
-        self.back_timer = current_timer
-
-        self.x +=  self.v * math.cos(self.t) * dt
-        self.y += self.v * math.sin(self.t) * dt
-        self.t += self.w * dt
-
-        odom = Odometry()
-        odom.header.stamp = current_timer.to_msg()
-        odom.header.frame_id = "odom"
-
-        odom.pose.pose.position.x = self.x
-        odom.pose.pose.position.y = self.y
-        odom.pose.pose.position.z = 0.0
-
-        odom.pose.pose.orientation.z = math.sin(self.t / 2.0)
-        odom.pose.pose.orientation.w = math.cos(self.t / 2.0)
-        """Quaternion has two parts:
-
-Vector part → (x, y, z)
-
-Scalar part → w
-
-For yaw rotation:
-
-Vector part lies on Z-axis → that’s your z
-
-w adjusts magnitude to keep rotation valid
-
-🔥 Important Understanding
-
-These are NOT:
-
-Not coordinates
-
-Not directions
-
-Not axes
-
-They are rotation encoding numbers.
-
-Think of them as compressed rotation data."""
-        odom.twist.twist.linear.x = self.v
-        odom.twist.twist.angular.z = self.w
-
-        self.get_logger().info(
-            f"x: {self.x:.3f}, y: {self.y:.3f}, theta: {self.t:.3f}"
-        )
-        self.odom_publisher.publish(odom)
-
-
-def main(args=None):
-    rclpy.init(args=args)
-    node = odometrynode()
-    rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()   
-
-if __name__ == '__main__':
-    main()
